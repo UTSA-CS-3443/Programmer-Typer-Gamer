9a39a4eab9c9981a50724fb5e70a1d81acfbabc6
Emiliano Rodriguez <31290912+MillzRodriguez@users.noreply.github.com>
2017-10-26 17:42:26 -0500

    Log in
    Added Login test
    type "user" to login

--- src/appController/MainController.java
+++ src/appController/MainController.java
@@ -1,15 +1,50 @@
-package appController;
+package application;
 
 import java.util.Random;
 
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label; 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage; 
 
 public class MainController {
-	
+	@FXML
+	private Label status; 
+	@FXML
+	private TextField user; 
 	@FXML
 	private Label myMessage; 
+	
+	
+	
+	public void Login(ActionEvent event) throws Exception {
+		//User login = "user"
+		if(user.getText().equals("user")) {
+			status.setText("Login Success");
+			
+			
+			
+			Stage secondaryStage = new Stage(); 
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Main.fxml"));
+			Scene scene = new Scene(root); 
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			secondaryStage.setTitle("Programmer-Typer-Gamer");
+			secondaryStage.setScene(scene);
+			secondaryStage.show();
+
+		
+		}else {
+			status.setText("Login Failed");
+		}
+	}
+
+
+	
 	public void generateRandom(ActionEvent event) {
 		Random rand = new Random();
 		int myRand = rand.nextInt(1000) + 1; 
