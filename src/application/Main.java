2ebca9201e06f90b9551252634757e3bdb5396f4
gener <gener@172.24.57.108>
2017-11-21 15:26:44 -0600

    added wordReader

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,18 +10,32 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader; 
+import javafx.fxml.FXMLLoader;
+//G.B. WordReader imports
+import java.util.Scanner;
+import javafx.beans.property.SimpleStringProperty;
 
 import controller.*;
 import view.*;
 import model.*;
-
 
 /*
  * Starts Login screen, user enters "user" to login
  */
 
 public class Main extends Application {
+	
+	//new block of WordReader lines
+	private static WordReader wordReader = new WordReader();
+	
+	/**
+	* The word to guess simple string property is a string which is an observable
+	* bean, so you can bind to it's value useful for communication between ui(view)
+	* and logic fields(model) properties allow you to not have to things manually,
+    */
+	
+	private static SimpleStringProperty word = new SimpleStringProperty();
+    //end block
 	
 
 	@Override
@@ -46,8 +60,31 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
+		
+		//wordReader.hard = true; //test case// is this setting it true before or after it goes through the if statements in WordReader
+		
+		//new block of WordReader lines
+		word.set(wordReader.getRandomWord().toUpperCase());
+        System.out.println(word);
+	     
+		System.out.println(wordReader.hard); //hard variable value test//
+		
+		// make a simple compare using scanner to take the user's input
+		Scanner scanner = new Scanner(System.in); //instead of system.in replace it with the file name
+		System.out.println("type the word on the sharks: ");
+		String typedWord = scanner.nextLine().toUpperCase();
+		System.out.println("Your typed word is: " + typedWord);
+		scanner.close(); 
+		
+		boolean match = false;
+		
+		if(word.getValue().equals(typedWord)) {
+			match = true;
+		}
+		
+		System.out.printf("match is: %s\n", match);
+		//end of block
+		
 		launch(args); 
 	}
-	
-	
 }
