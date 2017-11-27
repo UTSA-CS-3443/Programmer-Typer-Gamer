8af4c5efaf69465a4ee8c8e799a2fe78fbbc034a
Lucas Johns <Lucas Johns@172.24.120.53>
2017-11-26 20:44:48 -0600

    Asteroids

---
+++ src/application/controller/GameThreeController.java
@@ -0,0 +1,103 @@
+ package application.controller;
+
+import java.io.IOException;
+
+import application.model.GamemodeOne;
+import application.model.GamemodeThree;
+import application.model.GamemodeTwo;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class GameThreeController implements EventHandler<ActionEvent>{
+	
+	public static GamemodeThree mode;
+	@FXML public Canvas canvas;
+	@FXML public TextField wordField;
+	
+	@FXML public VBox pauseMenu;
+	@FXML public Button resumeButton;
+	@FXML public Button mainMenuButton;
+	@FXML public Button quitButton;
+	
+	@FXML public VBox gameOver3Menu;
+	@FXML public Button tryAgainButton;
+	@FXML public Button resetButton;
+	@FXML public Button quit2Button;
+
+	
+	public GameThreeController() {
+		super();
+	}
+	
+	public void initialize() {
+		mode = new GamemodeThree(this);
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		 if(event.getSource() instanceof TextField) {
+			 TextField field = (TextField)event.getSource();
+			 mode.checkWord(field.getText());
+			 field.clear();
+		 } 
+	}
+	
+	public void onKeyPressed(KeyEvent event) {
+		if(event.getCode() == KeyCode.ESCAPE) {
+			pause();
+		}
+	}
+	
+	public void pause() {
+		mode.setPaused(true);
+		pauseMenu.setVisible(true);
+		 
+	}
+	
+	public void resume() {
+		mode.setPaused(false);
+		pauseMenu.setVisible(false);
+	}
+	
+	public void retry() {
+		gameOver3Menu.setVisible(false);
+		mode = new GamemodeThree(this);
+	}
+	
+	public void mainMenu() {
+		Parent menuViewParent = null;
+		try {
+			menuViewParent = FXMLLoader.load(getClass().getResource("/application/view/Main.fxml"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} // Loads Main Menu fxml 
+		Scene menuViewScene = new Scene(menuViewParent);
+		
+		Stage window = (Stage) canvas.getScene().getWindow();
+		window.setScene(menuViewScene);
+		window.show(); 
+	}
+	
+	public void quit() {
+		System.exit(0);
+	}
+	
+	public void gameOver() {
+		mode.setPaused(true);
+		gameOver3Menu.setVisible(true);
+	}
+}
---
+++ src/application/model/Asteroid.java
@@ -0,0 +1,64 @@
+package application.model;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.text.Font;
+
+public class Asteroid {
+	private double x, y, dx, dy;
+	private String word;
+	private Image image;
+	private Image explo;
+	private Image heartImg; 
+	
+	public Asteroid(double x, double y, String word) {
+		this.x = x;
+		this.y = y;
+		this.word = word;
+		this.dx = -1;
+		this.dy = 0;
+		this.image = new Image("/Images/asteroid.png"); 
+		this.explo = new Image("/Images/explosion.png");
+		this.heartImg = new Image("/Images/heart.png" , 50, 50, false, false);
+
+	}
+	
+	public double getX() {
+		return x;
+	}
+	
+	public double getY() {
+		return y;
+	}
+	
+	public String getWord() {
+		return word;
+	}
+	
+	public void update() {
+		x += dx;
+		y += dy;
+	}
+	
+	public void draw(GraphicsContext gc) {
+		gc.drawImage(image, x, y);
+		gc.fillText(word, x + (image.getWidth() / 2)  - 15, y + (image.getHeight() / 2) + 10);
+	}
+	
+	public void explosion(GraphicsContext gc) {
+		gc.drawImage(explo, x - 150, y - 200);
+	}
+	
+	public void heart1(GraphicsContext gc) {
+		gc.drawImage(heartImg,5,5);
+	}
+	
+	public void heart2(GraphicsContext gc) {
+		gc.drawImage(heartImg,50,5);
+	}
+	
+	public void heart3(GraphicsContext gc) {
+		gc.drawImage(heartImg,95,5);
+	}
+
+}
